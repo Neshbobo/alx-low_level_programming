05d49047bf867ade0d62a73e3984d6afa4c44f7a
Neshbobo <wambuineema27@gmail.com>
2023-10-09 11:40:04 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,31 +13,22 @@ for (num1 = 0; num1 <= 99; num1++)
 {
 for (num2 = num1; num2 <= 99; num2++)
 {
-/* Check if the numbers are equal or have the same tens digit */
-if (num1 / 10 <= num2 / 10)
-{
-/* Print the tens digit of num1 (if it's zero, print '0') */
-putchar((num1 / 10 == 0) ? '0' : '0' + num1 / 10);
+/* Print the first two digits */
+putchar('0' + (num1 / 10));
+putchar('0' + (num1 % 10));
 
-/* Print the ones digit of num1 */
-putchar('0' + num1 % 10);
-
-/* Print a space between the two numbers */
+/* Print a space */
 putchar(' ');
 
-/* Print the tens digit of num2 (if it's zero, print '0') */
-putchar((num2 / 10 == 0) ? '0' : '0' + num2 / 10);
+/* Print the next two digits */
+putchar('0' + (num2 / 10));
+putchar('0' + (num2 % 10));
 
-/* Print the ones digit of num2 */
-putchar('0' + num2 % 10);
-
-/* If not the last combination, print comma and space */
+/* If not the last combination, print a comma and space */
 if (num1 != 99 || num2 != 99)
-
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
@@ -47,9 +38,4 @@ putchar('\n');
 
 return (0);
 }
-
-
-
-
-
 
