7695d46b41190070e6736f7d7cdc464c413dfa23
Neshbobo <wambuineema27@gmail.com>
2023-10-09 08:13:36 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char letter = 'z';
+
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
+}
+
+putchar('\n');
+
+return (0);
+}
+
+
+
+
+
+
+
