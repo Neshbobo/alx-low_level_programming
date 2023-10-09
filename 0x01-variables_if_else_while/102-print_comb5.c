b310c4608ad36b517c4efda2cdf4eeba02909c8d
Neshbobo <wambuineema27@gmail.com>
2023-10-09 06:13:23 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+* main - Entry point of the program
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 int num1, num2;
@@ -9,24 +14,26 @@ for (num1 = 0; num1 <= 99; num1++)
 for (num2 = num1; num2 <= 99; num2++)
 {
 /* Check if the numbers are equal or have the same tens digit */
-if (num1 != num2 && (num1 / 10) <= (num2 / 10))
+if (num1 / 10 <= num2 / 10)
 {
-/* Print the tens digit of num1 */
-putchar('0' + (num1 / 10));
+/* Print the tens digit of num1 (if it's zero, print '0') */
+putchar((num1 / 10 == 0) ? '0' : '0' + num1 / 10);
 
 /* Print the ones digit of num1 */
-putchar('0' + (num1 % 10));
+putchar('0' + num1 % 10);
 
-putchar(' '); /* Space between the two numbers */
+/* Print a space between the two numbers */
+putchar(' ');
 
-/* Print the tens digit of num2 */
-putchar('0' + (num2 / 10));
+/* Print the tens digit of num2 (if it's zero, print '0') */
+putchar((num2 / 10 == 0) ? '0' : '0' + num2 / 10);
 
 /* Print the ones digit of num2 */
-putchar('0' + (num2 % 10));
+putchar('0' + num2 % 10);
 
-/* Print comma and space if not the last combination */
-if (num1 != 98 || num2 != 99)
+/* If not the last combination, print comma and space */
+if (num1 != 99 || num2 != 99)
+
 {
 putchar(',');
 putchar(' ');
@@ -35,7 +42,8 @@ putchar(' ');
 }
 }
 
-putchar('\n'); /* Newline at the end */
+/* Print a newline character at the end */
+putchar('\n');
 
 return (0);
 }
