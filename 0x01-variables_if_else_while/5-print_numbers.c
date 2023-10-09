ff169507a78f8019a643858e10006d06547c52c8
Neshbobo <wambuineema27@gmail.com>
2023-10-09 08:21:43 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+printf("%d\n", i);
+}
+
+return (0);
+}
+
+
+
+
+
