9a02fff4c0a582694295e7a3c114f91c7cc78998
Neshbobo <wambuineema27@gmail.com>
2023-10-09 13:54:43 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,15 +21,15 @@ printf("The number %d is ", n);
 
 if (n > 0)
 {
-printf("%d is positive\n".n);
+printf("positive\n");
 }
 else if (n == 0)
 {
-printf("%d is zero\n".n);
+printf("zero\n");
 }
 else
 {
-printf("%d is negative\n".n);
+printf("negative\n");
 }
 
 return (0);
