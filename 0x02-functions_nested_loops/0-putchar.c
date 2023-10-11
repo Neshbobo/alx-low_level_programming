61598d477a0cfa374e615a14e03adf1b515e1719
Neshbobo <wambuineema27@gmail.com>
2023-10-11 09:51:13 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+/*
+* File: putchar_print.c
+* Author: Your Name
+* Date: October 11, 2023
+* Description: A program that prints "_putchar" and a new line, and returns 0.
+*/
+
+#include <stdio.h>
+
+/**
+* main - Entry point of the program.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+/*
+* Print "_putchar" followed by a new line.
+*/
+printf("_putchar\n");
+
+return (0);
+}
+
+
+
+
+
+
+
+
