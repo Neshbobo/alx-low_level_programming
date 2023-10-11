4a1f613750a2d4b43638948ccd1a863f7337ed89
Neshbobo <wambuineema27@gmail.com>
2023-10-11 10:04:20 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
 * main - Entry point
 *
@@ -8,6 +10,12 @@
 */
 int main(void)
 {
-write(1, "_putchar\n", 9);
+if (_putchar('_') == -1 || _putchar('p') == -1 || _putchar('u') == -1
+|| _putchar('t') == -1 || _putchar('c') == -1 || _putchar('h') == -1
+|| _putchar('a') == -1 || _putchar('r') == -1 || _putchar('\n') == -1)
+{
+return -1; /* Print error */
+}
+
 return (0);
 }
