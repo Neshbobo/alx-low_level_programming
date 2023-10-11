f5e0bcd3c678111d40e5b633e5c60c550a77d951
Neshbobo <wambuineema27@gmail.com>
2023-10-11 10:08:03 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,12 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
-* main - Entry point
+* _putchar - Custom putchar function
+* @c: The character to print
 *
-* Description: This program prints "_putchar" followed by a new line and
-*              returns 0.
-*
-* Return: Always 0
+* Return: 1 on success, -1 on error
 */
-int main(void)
-{
-if (_putchar('_') == -1 || _putchar('p') == -1 || _putchar('u') == -1
-|| _putchar('t') == -1 || _putchar('c') == -1 || _putchar('h') == -1
-|| _putchar('a') == -1 || _putchar('r') == -1 || _putchar('\n') == -1)
-{
-return -1; /* Print error */
-}
+int _putchar(char c);
 
-return (0);
-}
+#endif /* MAIN_H */
