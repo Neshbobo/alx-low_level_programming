d8d15178b082130fc4ce7b083216d9d661176eef
Neshbobo <wambuineema27@gmail.com>
2023-10-12 09:07:15 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,26 @@
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
- *
- * Description: This program prints "_putchar" followed by a new line and
- * returns 0.
- *
- * Return: 0 (Success)
- */
+* main - Entry point of the program
+*
+* Description: This program prints "_putchar" followed by a new line and
+* returns 0.
+*
+* Return: 0 (Success)
+*/
 int main(void)
 {
-    char *message = "_putchar\n";
+char *message = "_putchar\n";
 
-    while (*message)
-    {
-        write(1, message, 1);
-        message++;
-    }
-
-    return (0);
+while (*message)
+{
+write(1, message, 1);
+message++;
 }
+
+return (0);
+}
+
 
 
 
