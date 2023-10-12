e3afd16377298635cc04b8f00e165283a0aeb82f
Neshbobo <wambuineema27@gmail.com>
2023-10-12 09:01:31 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,28 @@
+#include <unistd.h>
+
 /**
-* main - Entry point of the program
-*
-* Description: This program prints "_putchar" followed by a new line.
-*
-* Return: 0 (Success)
-*/
+ * main - Entry point of the program
+ *
+ * Description: This program prints "_putchar" followed by a new line and
+ * returns 0.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-/*
-* We use the write function to print "_putchar" to the standard output.
-* The standard output is typically the console.
-*/
-write(1, "_putchar\n", 9);
+    char *message = "_putchar\n";
 
-return (0);
+    while (*message)
+    {
+        write(1, message, 1);
+        message++;
+    }
+
+    return (0);
 }
+
+
+
+
+
+
