b201d9434ee71d12730c06be24bd2fba4efab8b2
Neshbobo <wambuineema27@gmail.com>
2023-10-17 13:58:04 -0700

    101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,34 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 
-char* generatePassword(int length) {
-char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-int charsetLength = sizeof(charset) - 1;
-    
-char* password = (char*)malloc(length + 1);
-if (!password) {
-perror("Memory allocation error");
-exit(1);
-}
+/**
+* main - program that generates random valid
+* passwords for the program 101-crackme
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int pass[100];
+int i, sum, n;
 
-for (int i = 0; i < length; i++) {
-int randomIndex = rand() % charsetLength;
-password[i] = charset[randomIndex];
-}
+sum = 0;	
 
-password[length] = '\0';
-return password;
-}
-
-int main() {
 srand(time(NULL));
 
-int passwordLength = 12; // Adjust the length as needed
-char* password = generatePassword(passwordLength);
+for (i = 0; i < 100; i++)
+{
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - sum) - '0' < 78)
+{
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
+}
+}
 
-printf("Generated Password: %s\n", password);
-
-free(password);
-
-return 0;
+return (0);
 }
