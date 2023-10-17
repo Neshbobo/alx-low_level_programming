28c1283886bfb0814c57d1d3ac3a641c5811e35a
Neshbobo <wambuineema27@gmail.com>
2023-10-17 11:35:56 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,18 +4,23 @@
 * print_rev - prints a string in reverse, followed by a new line
 * @s: string to be printed
 */
-
 void print_rev(char *s)
 {
-int a, b, length;
-while(s[a] != '\0')
+int i, j, len;
+
+i = 0;
+
+while (s[i] != '\0')
 {
-a++;
+i++;
 }
-length = a;
-for(b = length - 1; b >= 0; b--)
+
+len = i;
+
+for (j = len - 1; j >= 0; j--)
 {
-_putchar(s[b]);
+_putchar(s[j]);
 }
+
 _putchar('\n');
 }
