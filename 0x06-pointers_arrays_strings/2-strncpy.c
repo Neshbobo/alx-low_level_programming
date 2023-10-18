3f350df05db952875f207bfa98e4973d38b419d8
Neshbobo <wambuineema27@gmail.com>
2023-10-18 12:17:03 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,32 +3,25 @@
 
 /**
 * _strncpy - Copy a string with a limit.
-* @dest: Pointer to destination string.
-* @src: Pointer to source string.
+* @dest: Pointer to the destination string.
+* @src: Pointer to the source string.
 * @n: Maximum number of bytes from src to copy.
 *
 * Return: A pointer to the resulting string (dest).
 */
-char *_strncpy(char *dest, const char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
-/* Copy up to 'n' bytes from src to dest */
 for (i = 0; i < n && src[i] != '\0'; i++)
+{
 dest[i] = src[i];
+}
 
-/* If n is not reached, pad the remaining space with null bytes */
-while (i < n)
+for (; i < n; i++)
 {
 dest[i] = '\0';
-i++;
 }
 
 return (dest);
 }
-
-
-
-
-
-
