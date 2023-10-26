acea8ed547575b9433ced28efe3b3d119a1ab6b9
Neshbobo <wambuineema27@gmail.com>
2023-10-26 12:53:51 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,28 @@
 #include "main.h"
 
 /**
-* is_prime_number - Checks if an integer is a prime number
-* @n: The integer to check
-*
-* Return: 1 if n is a prime number, 0 otherwise
+*prime1-is a subordinate function
+*@a: are integers
+*@b: are integers
+*Return: the answer
+*/
+int prime1(int a, int b)
+{
+if (a == b)
+return (1);
+else if (a % b == 0)
+return (0);
+return (prime1(a, b + 1));
+}
+
+/**
+*is_prime_number - is the main function
+*@n: an integer
+*Return: the answer
 */
 int is_prime_number(int n)
 {
 if (n <= 1)
 return (0);
-int i;
-for (i = 2; i * i <= n; i++)
-{
-if (n % i == 0)
-return (0);
-}
-return (1);
+return (prime1(n, 2));
 }
