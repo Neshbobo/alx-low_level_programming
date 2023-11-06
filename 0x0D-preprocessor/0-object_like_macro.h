30fc1b8ed53cee1d96e1f087cbf9d6f1c59b88f3
Neshbobo <wambuineema27@gmail.com>
2023-11-06 04:13:37 -0800

    0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+#ifndef SIZE_MACRO_H
+#define SIZE_MACRO_H
+
+/**
+* SIZE - A macro to define the size as 1024.
+*/
+#define SIZE 1024
+
+#endif /* SIZE_MACRO_H */
