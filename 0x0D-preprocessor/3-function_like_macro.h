8ead73bd46f7bb5dafb61579bd59398cd47c8e9a
Neshbobo <wambuineema27@gmail.com>
2023-11-06 04:48:09 -0800

    3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef ABS_MACRO_H
+#define ABS_MACRO_H
+
+/**
+* ABS - A macro that computes the absolute value of a number x.
+* @x: The number for which the absolute value is calculated.
+*
+* Return: The absolute value of x.
+*/
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* ABS_MACRO_H */
