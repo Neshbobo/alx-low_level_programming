4b98485610ed8a5b1361632e64c9ba16c3787229
Neshbobo <wambuineema27@gmail.com>
2023-11-06 04:38:41 -0800

    1

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,9 +1,11 @@
 #ifndef PI_MACRO_H
 #define PI_MACRO_H
 
-/**
-* PI_MACRO - A macro to define the mathematical constant pi.
+/*
+* PI_MACRO.H
+* A header file that defines PI as an abbreviation for 3.14159265359.
 */
-#define PI_MACRO 3.14159265359
+
+#define PI 3.14159265359
 
 #endif /* PI_MACRO_H */
