4feceda16a8899382481cc434b88aadafb8e7373
Neshbobo <wambuineema27@gmail.com>
2023-11-06 04:57:51 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_STRUCT_H
+#define DOG_STRUCT_H
+
+/**
+* struct dog - Represents information about a dog.
+* @name: The name of the dog.
+* @age: The age of the dog.
+* @owner: The owner of the dog.
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_STRUCT_H */
