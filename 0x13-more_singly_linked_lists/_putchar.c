5a24c766ac20284c195aec2600f6be99e431977f
Neshbobo <wambuineema27@gmail.com>
2023-11-20 12:04:51 -0800

    lists.h

---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+* _putchar - Writes a character to the standard output (stdout)
+* @c: The character to be written
+*
+* Return: On success, returns the written character. On error, returns -1.
+*/
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
+
