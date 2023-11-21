6b8feddfe7850f186c238fbfef06aa42df713d8a
Neshbobo <wambuineema27@gmail.com>
2023-11-21 01:04:33 -0800

    1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+ #include "lists.h"
 
 /**
 * listint_len - Returns the number of elements in a listint_t linked list.
@@ -19,26 +19,3 @@ count++;  /* Increment the count */
 return (count);  /* Return the number of nodes */
 }
 
-/**
-* main - Entry point of the program, demonstrating the listint_len function.
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
-{
-/* Create a sample linked list using the listint_t structure */
-listint_t node1 = {1, NULL};
-listint_t node2 = {2, NULL};
-listint_t node3 = {3, NULL};
-
-node1.next = &node2;
-node2.next = &node3;
-
-/* Get the number of elements in the linked list */
-size_t node_count = listint_len(&node1);
-
-/* Print the count */
-printf("Number of nodes: %lu\n", node_count);
-
-return (0);
-}
