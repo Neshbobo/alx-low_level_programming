ae8c9745bd29048b091a1a039150b6f32581da2b
Neshbobo <wambuineema27@gmail.com>
2023-11-21 02:40:58 -0800

    4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#inlude "lists.h"
+
+/**
+* free_listint - Frees a listint_t linked list.
+* @head: A pointer to the head of the list.
+*
+* Description: This function frees the memory allocated for each node
+*              in the list and sets the head pointer to NULL.
+*/
+void free_listint(listint_t *head)
+{
+listint_t *current;
+
+while (head != NULL)
+{
+current = head;
+head = head->next;
+free(current);
+}
+}
