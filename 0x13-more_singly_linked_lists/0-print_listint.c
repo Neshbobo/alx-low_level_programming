54dc5860a227c1aa3efb3f0e381b12fe642e42c5
Neshbobo <wambuineema27@gmail.com>
2023-11-21 02:13:15 -0800

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,38 +8,15 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t count = 0;  
+size_t count = 0;
 
 while (h != NULL)
 {
-printf("%d\n", h->n);  
-h = h->next;  
-count++;  
+printf("%d\n", h->n);
+h = h->next;
+count++;
 }
 
-return (count);  
+return (count);
 }
 
-/**
-* main - Entry point of the program, demonstrating the print_listint function.
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
-{
-/* Create a sample linked list */
-listint_t node1 = {1, NULL};
-listint_t node2 = {2, NULL};
-listint_t node3 = {3, NULL};
-
-node1.next = &node2;
-node2.next = &node3;
-
-/* Print the elements of the linked list and get the count */
-size_t node_count = print_listint(&node1);
-
-/* Print the count */
-printf("Number of nodes: %lu\n", node_count);
-
-return (0);
-}
