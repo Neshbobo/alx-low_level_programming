dc48bf33cc38dd9aa4e1684eac8916af48b3c9b8
Neshbobo <wambuineema27@gmail.com>
2023-11-28 05:09:06 -0800

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,5 +14,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+void print_error_and_exit(const char *msg);
 
 #endif
