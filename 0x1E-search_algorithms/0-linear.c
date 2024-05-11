5d820a23d0908d71bd331105f2636622086b6435
Neshbobo <wambuineema27@gmail.com>
2024-05-11 08:51:57 -0700

    0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,30 +1,26 @@
-#include <stdio.h>
 #include "search_algos.h"
 
-int linear_search(int *array, size_t size, int value);
+/**
+* linear_search - Implement the linear search algorithm
+* @array: Given array to search in
+* @size: Size of the array
+* @value: Value to search for
+* Return: First index or -1 if not found
+*/
 
-int main() {
-  int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  size_t size = sizeof(array) / sizeof(array[0]);
-  int value = 6;
-  int index = linear_search(array, size, value);
-  if (index != -1) {
-    printf("Value found at index %d\n", index);
-  } else {
-    printf("Value not found\n");
-  }
-  return 0;
+int linear_search(int *array, size_t size, int value)
+{
+if (array == NULL)
+{
+return (-1);
 }
-
-int linear_search(int *array, size_t size, int value) {
-  if (array == NULL) {
-    return -1;
-  }
-  for (size_t i = 0; i < size; i++) {
-    printf("Comparing %d to %d\n", array[i], value);
-    if (array[i] == value) {
-      return i;
-    }
-  }
-  return -1;
+for (size_t i = 0; i < size; i++)
+{
+printf("Comparing %d to %d\n", array[i], value);
+if (array[i] == value)
+{
+return (i);
+}
+}
+return (-1);
 }
