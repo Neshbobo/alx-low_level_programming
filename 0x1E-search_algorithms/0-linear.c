25cf1d55c4b285a32587804263b0476781e09c77
Neshbobo <wambuineema27@gmail.com>
2024-05-11 09:03:03 -0700

    0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,20 +7,19 @@
 * @value: Value to search for
 * Return: First index or -1 if not found
 */
-
 int linear_search(int *array, size_t size, int value)
 {
+size_t i;
+
 if (array == NULL)
-{
 return (-1);
-}
-for (size_t i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
-printf("Comparing %d to %d\n", array[i], value);
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 if (array[i] == value)
-{
 return (i);
-}
+
 }
 return (-1);
+
 }
