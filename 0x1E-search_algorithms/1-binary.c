49ec0d277e1a7c827687f7f67a7f986cfa00df33
Neshbobo <wambuineema27@gmail.com>
2024-05-11 10:30:24 -0700

    1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,54 +1,107 @@
 #include "search_algos.h"
 
+
+
+
 /**
-* binary_search - Searches for a value in a sorted array of integers
-*                using the binary search algorithm
-* @array: Pointer to the first element of the array to search in
-* @size: Number of elements in array
-* @value: Value to search for
-*
-* Return: The index where value is located, or -1 if value is not present
-*         in array or if array is NULL
-*/
+ * binary_recursive - Recursive approach for the binary search algorithm
+ * @array: Pointer to the first position
+ * @size: Length of the array
+ * @value: Element to be searched
+ * Return: Index of the value, or -1 if not found
+ */
+size_t binary_recursive(int *array, size_t size, int value)
+{
+	size_t mid = size / 2;
+	size_t aux;
+
+	if (!array || size == 0)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (aux = 0; aux < size; aux++)
+		printf("%d%s", array[aux], (aux != size - 1) ? ", " : "\n");
+
+	if (mid && size % 2 == 0)
+		mid--;
+	if (value == array[mid])
+		return (mid);
+	if (value < array[mid])
+		return (binary_recursive(array, mid, value));
+
+	mid++;
+	return (binary_recursive(array + mid, size - mid, value) + mid);
+}
+
+
+/**
+ * binary_search - searches for a value in a sorted array
+ * @array: pointer ti first position of array
+ * @size: length of array
+ * @value: value to search
+ * Return: index of value in array
+ */
 int binary_search(int *array, size_t size, int value)
 {
-if (array == NULL || size == 0)
-{
-return (-1);
+	int el;
+
+	el = binary_recursive(array, size, value);
+
+	if (el >= 0 && array[el] != value)
+		return (-1);
+	return (el);
 }
 
-size_t low = 0;
-size_t high = size - 1;
 
-printf("Searching in array: ");
-for (size_t i = low; i < high; i++)
-{
-printf("%d, ", array[i]);
-}
-printf("%d\n", array[high]);
 
-while (low <= high)
-{
-size_t mid = low + (high - low) / 2;
 
-if (array[mid] == value)
+
+
+
+100-jump.c
+
+
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - Implements the Jump search algorithm to
+ * find a value in an array of integers
+ *
+ * @array: Input array
+ * @size: Size of the array
+ * @value: Value to search for
+ * Return: Index of the number, or -1 if not found
+ */
+int jump_search(int *array, size_t size, int value)
 {
-return (mid);
-}
-else if (array[mid] < value)
-{
-low = mid + 1;
-}
-else
-{
-high = mid - 1;
-}
-printf("Searching in array: ");
-for (size_t i = low; i < high; i++)
-{
-printf("%d, ", array[i]);
-}
-printf("%d\n", array[high]);
-}
-return (-1);
+	int index, m, k, prev;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
+
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
+	}
+
+	return (-1);
 }
